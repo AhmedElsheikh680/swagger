21674efa6d53b8cba672bbd0e0f7b10c4744dac3
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-07-24 17:07:01 +0200

    @ApiModelProperty

--- src/main/java/com/spring/swagger/model/Employee.java
+++ src/main/java/com/spring/swagger/model/Employee.java
@@ -1,5 +1,6 @@
 package com.spring.swagger.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,8 +16,10 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
+    @ApiModelProperty(value = "This Is ID Of Employee")
     private int id;
 
+    @ApiModelProperty(value = "This Is Full Name Of Employee", required = true)
     @Column(name = "full_name")
     private String fullName;
 
