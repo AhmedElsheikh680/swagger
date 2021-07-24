02e0a943a48130d2fcf84cc3b86ba7a5d6651f25
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-07-24 17:33:02 +0200

    handle header of swagger using ApiInfo

--- src/main/java/com/spring/swagger/config/SwaggerConfig.java
+++ src/main/java/com/spring/swagger/config/SwaggerConfig.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
 
 @Configuration
 public class SwaggerConfig {
@@ -17,6 +21,23 @@ public class SwaggerConfig {
 //                .apis(RequestHandlerSelectors.any())
                     .apis(RequestHandlerSelectors.basePackage("com.spring.swagger"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(metaData());
+
+    }
+
+    private ApiInfo metaData(){
+        Contact contact =
+                new Contact("Ahmed Elsheikh", "https://www.facebook.com/profile.php?id=100007334026246", "ahmedelsheikh680@gmail.com");
+        return new ApiInfo(
+                "Spring Framework With Swagger",
+                "Employee & Player Controller",
+                "3.0.0",
+                "https://swagger.io/",
+                contact,
+                "License https://swagger.io/",
+                "https://swagger.io/",
+                new ArrayList<>()
+        );
     }
 }
